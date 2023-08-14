5d914d75a8055b5691a31ac4bced815dbadf9788
Tara Mehryar <tara.mehryar@gmail.com>
2023-08-14 09:39:37 -0500

    Wk 2 Day 1 - Find Lucky Numbers

---
+++ wk_2_day_1_findLuckyNumbers.js
@@ -0,0 +1,16 @@
+//Problem: Given a number, n, return an array containing n unique numbers between 1-10, inclusive. (Do not repeat any numbers in the list). You can trust function will never be called at n > 0 or n < 0.
+//  function listUniqueRandomNumbers(n) {
+//     const uniqueNumbers = new Set();
+//     while (uniqueNumbers.size < n){
+//         uniqueNumbers.add(Math.floor(Math.random() * 10) + 1);
+//     } return Array.from(uniqueNumbers);
+// }
+
+//SOLUTION GIVEN IN CLASS:
+function UniqueRandomNumbers(n) {
+const uniqueNumbers = [];
+for(let i = 0; i < Array.length; i++){
+    if ()
+}
+
+}
