623d10eb49ac0c7ba685f573c3f22c2d8e918152
Tara Mehryar <tara.mehryar@gmail.com>
2023-08-23 09:40:43 -0500

    Wk 3 Day 3

--- wk_3_day_3.js
+++ wk_3_day_3.js
@@ -9,10 +9,32 @@
 // Create a function called "hack". This function will receive an array and return it with all the user values updated to your information.
 // Use the map method.
 
-class User = {
-    this.firstName;
-    this.lastName;
-    this.username;
-    this.password;
-    this.email
+class User {
+    constructor(firstName, lastName, username, password, email){
+        this.firstName;
+        this.lastName;
+        this.username;
+        this.password;
+        this.email;
+        this.subscribe = false;
+    }
+
+    subscribe(){
+        this.subscribe = !this.subscribe;
+        return this.subscribe ? `${this.username} is now subscribed.` : `${this.username} is now unsubscribed.`;
+    }
+}
+
+const users = [
+    new User('tara','mehryar','tmehryar','password','tmehryar@icloud.com'),
+    new User('john', 'jacob', 'jjacob', 'passw0rd', 'jjacobs@icloud.com'),
+    new User(),
+    new User(),
+    new User()
+];
+
+function subscribeUsersWithStartingLetters(arr){
+    arr.forEach(user => {
+        const 
+    });
 }
