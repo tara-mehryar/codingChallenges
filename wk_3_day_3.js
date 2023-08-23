91030e70507fe981ce0e8e6a5e0ad95575849cab
Tara Mehryar <tara.mehryar@gmail.com>
2023-08-23 09:19:24 -0500

    Wk 3 Day 3

---
+++ wk_3_day_3.js
@@ -0,0 +1,18 @@
+// Create a User class. This class will receive the user's first & last name, a username, a password, and an email. It will also have a 
+// "subscribed" property which will be false by default.
+// Create a subscribe method. This method will set "subscribed" to true if it is false, and false if it is true, and return a 
+// string relevant to their new subscription status.
+// Instantiate 5 new Users, and push them into an array.
+// Create a function which receives an array. This function will call the subscribe method on any user whose name starts with "S", "M", or "L".
+// Use the forEach method.
+// Create a function which receives an array. This function will return a new array with only subscribed users.
+// Create a function called "hack". This function will receive an array and return it with all the user values updated to your information.
+// Use the map method.
+
+class User = {
+    this.firstName;
+    this.lastName;
+    this.username;
+    this.password;
+    this.email
+}
