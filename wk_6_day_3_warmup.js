6c9fc6ed75f20bbe6c936ec0fb2d9730c8422531
Tara Mehryar <tara.mehryar@gmail.com>
2023-09-13 09:06:40 -0500

    Wk 6 Day 3 - Warmup

---
+++ wk_6_day_3_warmup.js
@@ -0,0 +1,6 @@
+// Write a function that returns one of 3 different madLibs. The madLib string should contain placeholders for a noun,
+// adjective, verb, and color. Format these placeholders like this: #noun#
+
+
+// Write a function that receives a madLib, and a madLib object, containing the required key: value pairs.
+// Use the object values to replace the madLib placeholders, then return the completed madLib.
