7e29c706ff1f388c92de1c9d0b501a9633709089
Tara Mehryar <tara.mehryar@gmail.com>
2023-09-13 10:06:16 -0500

    Wk 6 Day 3 - Warmup

--- wk_6_day_3_warmup.js
+++ wk_6_day_3_warmup.js
@@ -1,25 +1,25 @@
 // Write a function that returns one of 3 different madLibs. The madLib string should contain placeholders for a noun,
 // adjective, verb, and color. Format these placeholders like this: #noun#
-function generateMadLib(){
-    const nouns = [];
-    const adjectives = [];
-    const verbs = [];
-    const colors = [];
+// function generateMadLib(){
+//     const nouns = [];
+//     const adjectives = [];
+//     const verbs = [];
+//     const colors = [];
 
-    const randomNoun = nouns[Math.floor(Math.random() * nouns.length)];
-    const randomAdjective = adjectives[Math.floor(Math.random() * adjectives.length)];
-    const randomVerb = verbs[Math.floor(Math.random() * verbs.length)];
-    const randomColor = colors[Math.floor(Math.random() * colors.length)];
+//     const randomNoun = nouns[Math.floor(Math.random() * nouns.length)];
+//     const randomAdjective = adjectives[Math.floor(Math.random() * adjectives.length)];
+//     const randomVerb = verbs[Math.floor(Math.random() * verbs.length)];
+//     const randomColor = colors[Math.floor(Math.random() * colors.length)];
 
-    const madLibs = [`The #color# #noun# likes to #verb# over the #adjective# lawn.`]
+//     const madLibs = [`The #color# #noun# likes to #verb# over the #adjective# lawn.`]
 
-    const randomMadLib = madLibs[Math.floor(Math.random() * madLibs.length)];
+//     const randomMadLib = madLibs[Math.floor(Math.random() * madLibs.length)];
 
-    return randomMadLib;
-}
+//     return randomMadLib;
+// }
 
-const randomMadLib = generateMadLib();
-console.log(randomMadLib);
+// const randomMadLib = generateMadLib();
+// console.log(randomMadLib);
 
 // Solution in class:
 const generateLib = () => {
@@ -31,9 +31,18 @@ const generateLib = () => {
     return madlibs[randIndex]
 }
 
-const createMadLib = (madlib,libObj) => {
+const createMadLib = (madlib,{ noun,color,adjective,verb }) => {
+    // let {noun,color,adjective,verb} = libObj
 
+    madlib = madlib.replace('#noun#', noun)
+    madlib = madlib.replace('#adjective#', adjective)
+    madlib = madlib.replace('#verb#', verb)
+    madlib = madlib.replace('#color#', color)
+
+    return madlib
 }
+
+console.log(createMadLib(generateLib(),{ noun:'cat', color:'orange', adjective:'fluffy', verb:'jump' }))
 
 // Write a function that receives a madLib, and a madLib object, containing the required key: value pairs.
 // Use the object values to replace the madLib placeholders, then return the completed madLib.
