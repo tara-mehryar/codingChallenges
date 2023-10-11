091e70ad242e78771d248be31d524e918c68a117
Tara Mehryar <tara.mehryar@gmail.com>
2023-10-11 09:15:55 -0500

    Warmup

---
+++ wk_9_day_3_warmup.js
@@ -0,0 +1,6 @@
+// Write a function that takes in a number. It should returna "tower" of * . The tower will be an array of strings.
+// The number passed in tells you how many floors the tower should have. 
+// The first floor should have 1 block, the 2nd should have 3 blocks, the 3rd should have 5 blocks, and so on. 
+// Each block is represented by a *.
+
+
