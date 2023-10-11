dd32ec329b645ebdb582dc24d0d8a7020cb1c6fe
Tara Mehryar <tara.mehryar@gmail.com>
2023-10-11 09:26:18 -0500

    warmup

--- wk_9_day_3_warmup.js
+++ wk_9_day_3_warmup.js
@@ -3,4 +3,21 @@
 // The first floor should have 1 block, the 2nd should have 3 blocks, the 3rd should have 5 blocks, and so on. 
 // Each block is represented by a *.
 
-
+function towerBuilder(nFloors) {
+    const tower = [];
+  
+    for (let i = 0; i < nFloors; i++) {
+      const spaces = ' '.repeat(nFloors - i - 1);
+      const blocks = '*'.repeat(2 * i + 1);
+      const floor = spaces + blocks + spaces;
+      tower.push(floor);
+    }
+  
+    return tower;
+  }
+  
+  // Example usage:
+  const numberOfFloors = 3;
+  const tower = towerBuilder(numberOfFloors);
+  console.log(tower);
+  
