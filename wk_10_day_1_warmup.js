6a9109a466ea0d5ef795461ae8225ebacbb4e521
Tara Mehryar <tara.mehryar@gmail.com>
2023-10-16 10:40:15 -0500

    Warmup

--- wk_10_day_1_warmup.js
+++ wk_10_day_1_warmup.js
@@ -46,38 +46,50 @@ console.log(timeWord("12:00"));  // 'noon'
 
 // In-class solution:
 const createHourStr = (hours) => {
-    if (hours > 11){
+    if(hours > 11){
         hours = hours - 12
     }
 
-    let hoursWords = [
-        "midnight", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "noon"
-    ];
-};
+    let hourWords = [
+        'twelve',
+        'one',
+        'two',
+        'three',
+        'four',
+        'five',
+        'six',
+        'seven',
+        'eight',
+        'nine',
+        'ten',
+        'eleven'
+    ]
+    return hourWords[hours]
+}
 
-const createMinutesStr = (minutes) => {
-    if (minutes < 20){
+const createMinuteStr = (minutes) => {
+    if(minutes < 20){
         let minuteArr = [
             "o'clock",
-            "oh one",
-            "oh two",
-            "oh three",
-            "oh four",
-            "oh five",
-            "oh six",
-            "oh seven",
-            "oh eight",
-            "oh nine",
+            "'oh one",
+            "'oh two",
+            "'oh three",
+            "'oh four",
+            "'oh five",
+            "'oh six",
+            "'oh seven",
+            "'oh eight",
+            "'oh nine",
             "ten",
             "eleven",
             "twelve",
-            "thirteen",
-            "fourteen",
-            "fifteen",
-            "sixteen",
-            "seventeen",
-            "eighteen",
-            "nineteen"
+            'thirteen',
+            'fourteen',
+            'fifteen',
+            'sixteen',
+            'seventeen',
+            'eighteen',
+            'nineteen'
         ]
         return minuteArr[minutes]
     } else {
@@ -100,25 +112,25 @@ const createMinutesStr = (minutes) => {
             'eight',
             'nine'
         ]
-        let tensPlace = parseInt(minuteVals[0] - 2)
+        let tensPlace = parseInt(minuteVals[0]) - 2
         let onesPlace = parseInt(minuteVals[1])
-        return `${tens[tensPlace]} ${ones[onesPlace]}}`
+        return `${tens[tensPlace]}${ones[onesPlace]}`
     }
-};
+}
 
 const timeWords = str => {
-    if (str === '12:00'){
+    if(str === '12:00'){
         return 'noon'
     } else if (str === '00:00'){
         return 'midnight'
     }
     let time = str.split(':')
 
-    let hours = createHourStr(parseInt(time[0]));
-    let minutes = createMinutesStr(parseInt(time[1]));
-    let timeOfDay = parseInt(time[0]) >= 12 ? 'pm' : 'am';
+    let hours = createHourStr(parseInt(time[0]))
+    let minutes = createMinuteStr(parseInt(time[1]))
+    let timeOfDay = parseInt(time[0]) >= 12 ? 'pm' : 'am'
 
     return `${hours} ${minutes} ${timeOfDay}`
-};
+}
 
-console.log(timeWords('15:57'));
+console.log(timeWords('15:45'))
